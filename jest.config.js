4331ff3c3565682b85a185db6ebd586857d4c01c
Alexey <nerjs.stap@gmail.com>
2024-12-14 18:30:34 +0100

    changes setup/ Move to typescript

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+
+
+/** @type {import('jest').Config} */
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: { '^.+\\.ts$': 'ts-jest' },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: ['<rootDir>/**/*.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', '__fixtures__'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['html', 'json'],
+  testTimeout: 20_000,
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  maxConcurrency: 5,
+  testPathIgnorePatterns: ['/node_modules/', '/dist/*'],
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+}
